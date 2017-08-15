2bef21640d54ef5da1bd7489bbededc414f29f30
Aaron Anderson <aaronlynanderson@gmail.com>
2017-08-15 14:16:59 -0400

    subsubinitial commit

--- our-first-app/app.js
+++ our-first-app/app.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+const mustache = require('mustache-express');
 
-// app.get('/',function(req, res) {
-//   res.send("Hello world!");
+app.engine('mustache', mustache());
+app.set('vews', '/views');
+app.set('view engine', 'mustage');
+app.use('/our-first-app', express.static('our-first-app'));
+
+app.get('/',function(req, res) {
+  res.send("Hello world!");
+});
+
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname+'/index.html'));
 // });
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname+'/index.html'));
+app.get('/:something', function(req, res){
+  res.send(`The thing you wrote was ${req.params.something}`);
+});
+
+app.get('/template', function(req, res){
+  app.render('index', {name: 'Lila'});
 });
 
 app.listen(port, function(){

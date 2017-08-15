e2f977d297d7f1e27d3764d6b3a42ef7427e49a9
Aaron Anderson <aaronlynanderson@gmail.com>
2017-08-15 11:48:05 -0400

    subinitial commit

--- our-first-app/app.js
+++ our-first-app/app.js
@@ -1,2 +1,15 @@
 const express = require('express');
 const app = express();
+const port = 3000;
+
+// app.get('/',function(req, res) {
+//   res.send("Hello world!");
+// });
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.listen(port, function(){
+  console.log("The app is running on port 3000");
+});

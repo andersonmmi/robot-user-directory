e521d1a71a38ee8f175683f0c688b99ecb0501dd
Aaron Anderson <aaronlynanderson@gmail.com>
2017-08-16 15:08:35 -0400

    reposit our-first-app in current form

--- our-first-app/app.js
+++ our-first-app/app.js
@@ -4,26 +4,27 @@ const port = 3000;
 const path = require('path');
 const mustache = require('mustache-express');
 
-app.engine('mustache', mustache());
-app.set('vews', '/views');
-app.set('view engine', 'mustage');
+app.engine('mustache', mustache-express());
+app.set('vews', './views');
+app.set('view engine', 'mustache');
+
 app.use('/our-first-app', express.static('our-first-app'));
 
-app.get('/',function(req, res) {
-  res.send("Hello world!");
-});
+// app.get('/',function(req, res) {
+//   res.send("Hello world!");
+// });
 
 // app.get('/', function(req, res) {
 //     res.sendFile(path.join(__dirname+'/index.html'));
 // });
 
-app.get('/:something', function(req, res){
-  res.send(`The thing you wrote was ${req.params.something}`);
+app.get('/', function(req, res){
+  app.render('index', { name: 'Lila' } );
 });
 
-app.get('/template', function(req, res){
-  app.render('index', {name: 'Lila'});
-});
+// app.get('/:something', function(req, res){
+//   res.send(`The thing you wrote was ${req.params.something}`);
+// });
 
 app.listen(port, function(){
   console.log("The app is running on port 3000");
